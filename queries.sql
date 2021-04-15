37ba6be0261ddbaf78957608b4e0f46610baab5f
unknown <phungkhanhlinh.iu@gmail.com>
2021-04-15 15:55:44 +0700

    add other half query

--- queries.sql
+++ queries.sql
@@ -3,17 +3,38 @@ use tutorweb;
 SELECT * FROM tutor;
 SELECT * FROM tutee;
 SELECT * FROM admin;
-SELECT * FROM tutor WHERE Id = Id?;
-SELECT * FROM tutee WHERE Id = Id?;
-SELECT * FROM admin WHERE Id = Id?;
-SELECT * FROM tutor WHERE UserName = userName?;
-SELECT * FROM tutee WHERE UserName = userName?;
-SELECT * FROM admin WHERE UserName = userName?;
-
+SELECT * FROM tutor WHERE Id = ?;
+SELECT * FROM tutee WHERE Id = ?;
+SELECT * FROM admin WHERE Id = ?;
+SELECT * FROM tutor WHERE UserName =?;
+SELECT * FROM tutee WHERE UserName =?;
+SELECT * FROM admin WHERE UserName =?;
+/****************/
 SELECT * FROM contract;
-SELECT * FROM contract WHERE Id = Id?;
-SELECT * FROM contract WHERE TutorId = tutorId? AND TuteeId = tuteeId?;
+SELECT * FROM contract WHERE Id = ?;
+SELECT * FROM contract WHERE TutorId = ? AND TuteeId = ?;
 SELECT * FROM issue;
-SELECT * FROM issue WHERE Id = Id?;
-SELECT * FROM issue WHERE ContractId = Id?;
-SELECT * FROM issue WHERE ResolveAdminId = Id?;
+SELECT * FROM issue WHERE Id = ?;
+SELECT * FROM issue WHERE ContractId = ?;
+SELECT * FROM issue WHERE ResolveAdminId = ?;
+/****************/
+SELECT * FROM MoneyAccount WHERE Code=?;
+/* implement in functions: 
+    if (tutor)
+        para='tutor/'+tutorId
+    else 
+        para='tutee/'+tuteeId
+*/ 
+SELECT * FROM MoneyAccount WHERE Id=?;
+SELECT * FROM Transaction;
+SELECT * FROM Transaction WHERE Id=?;
+SELECT * FROM Transaction WHERE SenderAccountId=?;
+SELECT * FROM Transaction WHERE ReceiverAccountId=?;
+SELECT * FROM Transaction WHERE SenderAccountId=? AND ReceiverAccountId =?; 
+/****************/
+SELECT * FROM Chatroom WHERE Id=?;
+SELECT * FROM Chatroom WHERE TutorId =? AND TuteeId=?;
+SELECT * FROM Message WHERE ChatroomId=?;
+SELECT * FROM Message WHERE ChatroomId=? and IsTutor=1; /* 1 is tutor*/
+SELECT * FROM Message WHERE ChatroomId=? and IsTutor=0;
+
