eeefb3cf6e0d119566312ffe012b0282ac707920
Kazehaya Khoa <dangkhoavippro99x@gmail.com>
2021-04-30 01:28:23 +0700

    update update_service

--- app/Models/update_service.js
+++ app/Models/update_service.js
@@ -16,10 +16,10 @@ module.exports = {
     },
     /*used by admins only*/
     async addCourse(name) {
-        await Database.raw(`INSERT INTO Course VALUES(?)`, [name])
+        await Database.raw(`INSERT INTO Course (Name) VALUES(?)`, [name])
     },
     async addAdmin(adminInfo) {
-        await Database.raw(`INSERT INTO Admin VALUES(?,?,?,?,?,?)`, [adminInfo.FirstName, adminInfo.LastName, adminInfo.UserName,
+        await Database.raw(`INSERT INTO Admin (FirstName, LastName, UserName, Email, Password, DateofBirth) VALUES(?,?,?,?,?,?)`, [adminInfo.FirstName, adminInfo.LastName, adminInfo.UserName,
         adminInfo.Password, adminInfo.DateOfBirth])
     },
     async deleteTutor(tutorId) {
@@ -38,23 +38,23 @@ module.exports = {
 
     /*==================*/
     async addContract(contract) {
-        await Database.raw(`INSERT INTO contract VALUES(?,?,?,?,?,?,?)`, [parseInt(contract.tutorId), parseInt(contract.tuteeId),
+        await Database.raw(`INSERT INTO contract (TutorId, TuteeId, StartDate, CloseDate, TeachingHours, State, ListOfTeachingDay) VALUES(?,?,?,?,?,?,?)`, [parseInt(contract.tutorId), parseInt(contract.tuteeId),
         contract.startDate, contract.closeDate, parseFloat(teachingHours), 0, contract.listofTeachingDay]);
     },
     async addMoneyAccountByContractId(contractId) {
         let code = 'contract' + contractId;
-        await Database.raw(`INSERT INTO MoneyAccount VALUES(?,?)`, [code, 0])
+        await Database.raw(`INSERT INTO MoneyAccount (Code, BalanceAmount) VALUES(?,?)`, [code, 0])
     },
     async addMoneyAccountByTutorId(tutorId) {
         let code = 'tutor/' + tutorId;
-        await Database.raw(`INSERT INTO MoneyAccount VALUES(?,?)`, [code, 0])
+        await Database.raw(`INSERT INTO MoneyAccount (Code, BalanceAmount) VALUES(?,?)`, [code, 0])
     },
     async addMoneyAccountByTuteeId(tuteeId) {
         let code = 'tutee/' + tuteeId;
-        await Database.raw(`INSERT INTO MoneyAccount VALUES(?,?)`, [code, 0])
+        await Database.raw(`INSERT INTO MoneyAccount (Code, BalanceAmount) VALUES(?,?)`, [code, 0])
     },
     async addTransaction(transaction) {
-        await Database.raw(`INSERT INTO transaction VALUES(?,?,?)`, [parseInt(transaction.senderAccountId), parseInt(transaction.receiverAccountId), parseFloat(amount)]);
+        await Database.raw(`INSERT INTO transaction (Code, BalanceAmount) VALUES(?,?,?)`, [parseInt(transaction.senderAccountId), parseInt(transaction.receiverAccountId), parseFloat(amount)]);
     },
     async updateMoneyAccount(moneyAccount) {
         await Database.raw(`UPDATE MoneyAccount SET balanceAmount=? WHERE Id =?`, [parseFloat(moneyAccount.balanceAmount), parseInt(moneyAccount.Id)])
